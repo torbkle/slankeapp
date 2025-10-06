2d2f738b43372a17d7649e7f65dcaa950068cc77
torbkle <torkle05553@stud.noroff.no>
2025-10-06 09:50:36 +0200

    Update supabase_klient.py

--- supabase_klient.py
+++ supabase_klient.py
@@ -1,8 +1,11 @@
-# supabase_klient.py
 from supabase import create_client
+from dotenv import load_dotenv
 import os
 
-SUPABASE_URL = os.getenv("SUPABASE_URL", "https://zewmjurylmyjweyqotpw.supabase.co")
-SUPABASE_KEY = os.getenv("SUPABASE_KEY", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inpld21qdXJ5bG15andleXFvdHB3Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc1OTQwMDA5NiwiZXhwIjoyMDc0OTc2MDk2fQ.8DfBdida1EE9RggZQnkARzvod2XR00isq8cXHXOWaQ8")
+load_dotenv()
 
-supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
+url = os.getenv("SUPABASE_URL")
+public_key = os.getenv("SUPABASE_PUBLIC_KEY")
+
+supabase = create_client(url, public_key)
+
